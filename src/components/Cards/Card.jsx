9f645a1cbb7f06f96875753070d543a789d13e99
Vitor Nogueira <vslnogueira96@gmail.com>
2022-12-09 16:18:44 -0300

    done: Inserindo informações no component Card

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import './Card.css'
 
 export default class Card extends Component {
   render() {
     return (
-      <div>Card</div>
+      <div className="container-card">
+        <header className="header">
+          <p>entrada</p>
+          <img src="icon" alt="icon" />
+          <span>1000</span>
+        </header>
+      </div>
     )
   }
 }
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import Card from '../Cards/Card'
 import './Resume.css'
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa"
 
 export default class Resume extends Component {
   render() {
     return (
       <div className="container-resume">
-        Olá
+        <Card />
+        <Card />
+        <Card />
       </div>
     )
   }
