01adfe7d51fda867516df5b4636698e68b29e1dc
Vitor Nogueira <vslnogueira96@gmail.com>
2022-12-18 10:45:45 -0300

    Arquivos finais projeto Financial Control

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './Header.css'
+import { FaDollarSign } from "react-icons/fa"
 
 
 export default class Header extends Component {
@@ -7,7 +8,7 @@ export default class Header extends Component {
     return (
       <div className='container-header'>
         <div className="header"> 
-          <h1 className="title"> Controle Financeiro </h1> 
+           <h1 className="title"> <FaDollarSign/> Controle Financeiro </h1> 
         </div>
       </div>
     )
