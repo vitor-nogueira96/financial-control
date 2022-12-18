a5db27ed37b5e6d718bd49563777466e9cbcfc46
Vitor Nogueira <vslnogueira96@gmail.com>
2022-12-18 11:01:12 -0300

    Arquivos finais projeto Financial Control

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,58 +1,58 @@
-import React, { useState } from 'react'
-import Grid from '../Grid/Grid';
-import './Form.css'
-
+import React, { useState } from "react";
+import Grid from "../Grid/Grid";
+import "./Form.css";
 
 const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
-    const [desc, setDesc] = useState("");
-    const [amount, setAmount] = useState("");
-    const [isExpense, setExpense] = useState(false)
+  const [desc, setDesc] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isExpense, setExpense] = useState(false);
 
-    const generateID = () => Math.round(Math.random() * 1000);
+  const generateID = () => Math.round(Math.random() * 1000);
 
-    const handleSave = () => {
-      if (!desc || !amount) {
-        alert("Informe a descrição e o valor!");
-        return;
-      } else if (amount < 1) {
-        alert("O valor tem que ser positivo");
-        return;
-      }
+  const handleSave = () => {
+    if (!desc || !amount) {
+      alert("Informe a descrição e o valor!");
+      return;
+    } else if (amount < 1) {
+      alert("O valor tem que ser positivo");
+      return;
+    }
 
-      const transaction = {
-        id: generateID(),
-        desc: desc,
-        amount: amount,
-        expense: isExpense,
-      }
-
-      handleAdd(transaction);
-
-      setDesc("");
-      setAmount("");
+    const transaction = {
+      id: generateID(),
+      desc: desc,
+      amount: amount,
+      expense: isExpense,
     };
 
-    return (
-      <><div className="container-form">
+    handleAdd(transaction);
 
+    setDesc("");
+    setAmount("");
+  };
+
+  return (
+    <>
+      <div className="container-form">
         <div className="input-content-description">
           <input
             type="text"
             className="description"
-            placeholder='Descrição'
+            placeholder="Descrição"
             value={desc}
-            onChange={(event) => setDesc(event.target.value)} />
+            onChange={(event) => setDesc(event.target.value)}
+          />
         </div>
 
         <div className="input-content-value">
           <input
             type="number"
             className="description"
-            placeholder='Valor'
+            placeholder="Valor"
             value={amount}
-            onChange={(event) => setAmount(event.target.value)} />
+            onChange={(event) => setAmount(event.target.value)}
+          />
         </div>
-
         <div className="container-radio">
           <label htmlFor="rIncome">
             <input
@@ -60,23 +60,25 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
               name="group1"
               id="rIncome"
               defaultChecked
-              onChange={() => setExpense(!isExpense)} /> Entrada
+              onChange={() => setExpense(!isExpense)}
+            />{" "}
+            Entrada
           </label>
-
           <label htmlFor="rExpenses">
             <input
               type="radio"
               name="group1"
               id="rExpenses"
-              onChange={() => setExpense(!isExpense)} /> Saída
+              onChange={() => setExpense(!isExpense)}
+            />{" "}
+            Saída
           </label>
         </div>
         <button onClick={handleSave}>Salvar</button>
       </div>
-      <Grid itens={transactionsList} setItens={setTransactionsList}/>
-      </>
-  )
-}
+      <Grid itens={transactionsList} setItens={setTransactionsList} />
+    </>
+  );
+};
 
 export default Form;
-
